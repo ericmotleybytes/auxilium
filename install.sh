79c158a8bc339cbfd6420e711600b31f41394f55
Eric Alan Christiansen <eric@motleybytes.com>
2017-06-22 22:54:14 -0700

    adding auxsource

--- install.sh
+++ install.sh
@@ -9,10 +9,14 @@ if [ -z "$1" ]; then
     exit 1
 fi
 tgtdir="$1"
-auxdir=${BASH_SOURCE[0]%/*}   # keep just dir part
-echo "INFO: Installing $auxdir/bin/auxenv to $tgtdir/bin/auxenv..."
+script=$(readlink -f ${BASH_SOURCE[0]})
+auxdir=$(dirname "$script")
 mkdir -p "$tgtdir"/bin
-cp "$auxdir/bin/auxenv" "$tgtdir/bin/auxenv"
-chmod +xr "$tgtdir/bin/auxenv"
-echo "INFO: Completed auxilium installation successfully."
+for file in bin/auxenv bin/auxsource; do
+    echo "INFO: Installing $auxdir/$file to $tgtdir/$file..."
+    cp "$auxdir/$file" "$tgtdir/$file"
+    chmod a+r "$tgtdir/$file"
+    chmod a-x "$tgtdir/$file"
+done
+echo "INFO: Completed auxilium installation."
 exit 0
